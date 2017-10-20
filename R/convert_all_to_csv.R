517721c5fdb43c6e7777c0b81419070b3f18bb91
Apoorva Lal <lal.apoorva@gmail.com>
2017-10-20 00:51:42 -0700

    added fn to bulk write files to csv

---
+++ R/convert_all_to_csv.R
@@ -0,0 +1,18 @@
+####################################
+#' Writes all dataframes to csv
+#' @param list of dataframes, path of file/s
+#' @export
+#' @keywords write save bulk
+#' @examples
+#' convert_all_to_csv(dataframes, path)
+
+####################################
+# writes all dataframes to csv
+####################################
+
+convert_all_to_csv <- function(dataframes,path) {
+    for (file in dataframes){
+        write.csv(mget(file, .GlobalEnv),
+            paste0(path,'/',file,'.csv'))
+    }
+}
