6489a0175bac24cb3374c80e26af779874b84a32
apoorvalal <lal.apoorva@gmail.com>
2018-05-02 00:12:15 -0700

    update

--- R/fuzzy_match.R
+++ R/fuzzy_match.R
@@ -1,7 +1,6 @@
 #' fuzzy matches 2 character vectors, and returns most similar pairs
 #' @param character vectors a and b
 #' @keywords fuzzy match string
-#' @export dataframe
 #' @examples
 #' fuzzy_match(keys_A, keys_B)
 fuzzy_match <- function(a,b) {
@@ -11,6 +10,6 @@ fuzzy_match <- function(a,b) {
   # find the closest match for each
   match <- apply(distance, 1, which.min)
   key = a
-  values = b[match] # slice b using match index 
+  values = b[match] # slice b using match index
   return(as.data.frame(cbind(key, values)))
 }
