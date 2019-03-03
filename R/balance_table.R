115fd8506ac2ca945c5031c93fa086994c897a5f
Apoorva Lal <lal.apoorva@gmail.com>
2019-03-03 09:41:42 -0800

    update

--- R/balance_table.R
+++ R/balance_table.R
@@ -1,4 +1,4 @@
-#' Balance Check for observational studies
+#' Balance Check for observational studies; includes SD and Normalised Difference
 #' @param df dataframe to check balance
 #' @param treatvar dummy for treatment
 #' @param bal_vars covariates to check balance in
@@ -6,18 +6,30 @@
 #' @export
 #' @examples
 #' balance_table(nsw_psid, 'nsw', c('age', 'black', 'u75'))
-balance_table = function(df,
-    treatvar, bal_vars){
-  index_bal = which(names(df) %in% bal_vars)
-  bal_mat = matrix(NA, ncol = 4, nrow = length(index_bal))
-  for (i in 1:length(index_bal)){
-    t = t.test(df[,index_bal[i]] ~ df[, treatvar], na.rm = TRUE)
-    bal_mat[i, 1] = round(t$estimate[1], digits = 3)
-    bal_mat[i, 2] = round(t$estimate[2], digits = 3)
-    bal_mat[i, 3] = round(t$estimate[2] - t$estimate[1], digits = 3)
-    bal_mat[i, 4] = round(t$p.value, digits = 3)
-  }
-  rownames(bal_mat) = names(df)[index_bal]
-  colnames(bal_mat) = c("Control Mean", "Treatment Mean", "Difference in Means", "p-value")
-  format(bal_mat, digits = 4)
+balance_table <- function (df, treatvar, bal_vars)  {
+    treat_all   = df[which(df[, treatvar] == 1), bal_vars]
+    control_all = df[which(df[, treatvar] == 1), bal_vars]
+    bal_mat = matrix(NA, ncol = 7, nrow = length(bal_vars))
+    i = 1
+    for (bv in bal_vars) {
+        treat   = na.omit(treat_all[, bv])
+        control = na.omit(control_all[, bv])
+        # t test output
+        t = t.test(df[, bv] ~ df[, treatvar], na.rm = TRUE)
+        bal_mat[i, 1] = t$estimate[1]
+        bal_mat[i, 3] = t$estimate[2]
+        bal_mat[i, 5] = t$estimate[2] - t$estimate[1]
+        bal_mat[i, 6] = t$p.value
+        # sd and normalised difference
+        bal_mat[i, 2] = sd(control)/sqrt(length(control))
+        bal_mat[i, 4] = sd(treat)/sqrt(length(treat))
+        # normalised difference
+        bal_mat[i, 7] = (bal_mat[i, 5]/ sqrt(sd(control)^2 + sd(treat)^2)/2)
+        i = i+1
+      }
+    rownames(bal_mat) = bal_vars
+    colnames(bal_mat) = c("Control Mean", "Control SE", "Treatment
+      Mean", "Treatment SE", "Difference in Means", "p-value", "Normalised Difference")
+    bal_mat = round(bal_mat, digits = 4)
+    bal_mat
 }
