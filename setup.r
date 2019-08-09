5ba50ca920688baa0c77d7b870b41610773560db
Apoorva Lal <lal.apoorva@gmail.com>
2019-08-09 11:08:38 -0700

    "added dependencies"
    "added full list of dependencies"
    
    Added dependencies and updated description

--- setup.r
+++ setup.r
@@ -2,12 +2,14 @@
 rm(list=ls())
 # Run this to install new version after pushing to github
 load_or_install(c('devtools','roxygen2'))
+
 ####################################################
 #%% Run this to document new functions and populate namespace
 home = "~/Desktop/code/LalRUtils/"
 setwd(home)
 # create('LalRUtils') # only run once
 document()
+
 #%%
 # reinstall after pushing updates
 library(devtools)
