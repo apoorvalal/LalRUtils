ef888486217dc7efb5198ee183a5dad939766a02
apoorvalal <lal.apoorva@gmail.com>
2019-10-20 18:39:03 -0700

    "added package libreq 2"

---
+++ R/libreq.R
@@ -0,0 +1,15 @@
+#' Loads a list of libraries, installs when necessary - Take 2
+#' @param
+#' @param repo string containing repo to be passed to install.packages
+#' @keywords libraries install
+#' @export
+#' @examples
+#' libreq(tidyverse, lfe, estimatr))
+
+libreq <- function(..., repo='https://cloud.r-project.org/') {
+    wants = as.character(match.call(expand.dots = FALSE)[[2]])
+    has <- wants %in% rownames(installed.packages())
+    if(any(!has)) install.packages(wants[!has], repos=repo,dependencies=TRUE)
+    loaded = suppressMessages(lapply(wants,require,character.only=TRUE))
+    print(cbind(wants, loaded))
+}
