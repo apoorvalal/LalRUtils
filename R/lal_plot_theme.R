2dfe7ebeefa35615c2c86014b21e2bec41af4973
Apoorva Lal <lal.apoorva@gmail.com>
2020-04-15 23:23:31 -0700

    "restoring grid lines in default plot theme"

--- R/lal_plot_theme.R
+++ R/lal_plot_theme.R
@@ -6,7 +6,7 @@
 #' plot_ggp_object + lal_plot_theme()
 
 lal_plot_theme <- function(fontfam = "IBM Plex Sans Condensed", fsize = 15, textangle = 0,...) {
-  theme_classic() +
+  theme_bw() +
   theme(
     text = element_text(family = fontfam, size = fsize),
     axis.text.x = element_text(angle = textangle),
