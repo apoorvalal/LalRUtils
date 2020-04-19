74036b94f23e902a1024f182bf5cd9cc657754e7
Apoorva Lal <lal.apoorva@gmail.com>
2020-04-19 11:06:35 -0700

    "cleaned up themes"

--- R/lal_plot_theme.R
+++ R/lal_plot_theme.R
@@ -1,4 +1,4 @@
-#' Dark theme for nice looking plots with sensible defaults (no legend title, legend at the bottom)
+#' theme for nice looking plots with sensible defaults (no legend title, legend at the bottom)
 #' @param ggplot object
 #' @keywords ggplot plots
 #' @export
@@ -6,11 +6,11 @@
 #' plot_ggp_object + lal_plot_theme()
 
 lal_plot_theme <- function(fontfam = "IBM Plex Sans Condensed", fsize = 15, textangle = 0,...) {
-  theme_bw() +
+  theme_minimal() +
   theme(
     text = element_text(family = fontfam, size = fsize),
     axis.text.x = element_text(angle = textangle),
-    legend.title = element_blank(),
+    # legend.title = element_blank(),
     legend.position='bottom',
     panel.border = element_blank(),
     ...
--- R/lal_plot_theme_d.R
+++ R/lal_plot_theme_d.R
@@ -1,17 +1,19 @@
-#' Fork of hrbrthemes::theme_ft_rc() with altered legend options and dots
+#' Wrapper for hrbrthemes::theme_ft_rc() with bigger font, altered legend options and dots
 #' @param ggplot object
 #' @keywords ggplot plots
 #' @export
 #' @examples
 #' plot_ggp_object + lal_plot_theme_d()
 
-lal_plot_theme_d <- function(fontfam = "IBM Plex Sans Condensed", fsize = 15, textangle = 0,...) {
+lal_plot_theme_d <- function(fontfam = "IBM Plex Sans Condensed", fsize = 15, axsize = 13, textangle = 0, ...) {
   suppressMessages(library(hrbrthemes))
-  theme_ft_rc() +
+  theme_modern_rc(base_family = fontfam, subtitle_family = fontfam,
+    axis_title_just = 'c', base_size = fsize) +
   theme(
     text = element_text(family = fontfam, size = fsize),
-    axis.text.x = element_text(angle = textangle),
-    legend.title = element_blank(),
+    axis.text.x = element_text(angle = textangle, size = axsize),
+    axis.text.y = element_text(face = "bold", size = axsize),
+    # legend.title = element_blank(),
     legend.position='bottom',
     panel.border = element_blank(),
     ...
