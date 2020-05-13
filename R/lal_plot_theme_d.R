b62080f72bdc9f98a8fc8a0577bd703429873e1f
Apoorva Lal <lal.apoorva@gmail.com>
2020-05-12 21:57:11 -0700

    "fixes to panel text colour"

--- R/lal_plot_theme_d.R
+++ R/lal_plot_theme_d.R
@@ -7,13 +7,14 @@
 
 lal_plot_theme_d <- function(fontfam = "IBM Plex Sans Condensed", fsize = 15, axsize = 13, textangle = 0, ...) {
   suppressMessages(library(hrbrthemes))
-  theme_modern_rc(base_family = fontfam, subtitle_family = fontfam,
+  theme_ft_rc(base_family = fontfam, subtitle_family = fontfam,
     axis_title_just = 'c', base_size = fsize) +
   theme(
     text = element_text(family = fontfam, size = fsize),
     axis.text.x = element_text(angle = textangle, size = axsize),
     axis.text.y = element_text(face = "bold", size = axsize),
     # legend.title = element_blank(),
+    strip.text = element_text(color = 'white'),
     legend.position='bottom',
     panel.border = element_blank(),
     ...
