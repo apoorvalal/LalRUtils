6cb226b81711c090180e944ac87506fccdeb0725
Apoorva Lal <lal.apoorva@gmail.com>
2020-07-26 20:19:19 -0700

    "trying pkgdown"

--- R/oneliners.R
+++ R/oneliners.R
@@ -4,3 +4,11 @@ nunique = function(x) length(unique(x))
 
 #' @export
 lv = function() .Last.value
+
+#' @export
+mMscale <- function(X){
+  X = as.matrix(X)
+  mins = apply(X,2,min)
+  maxs = apply(X,2,max)
+  return(scale(X, center=mins, scale=maxs-mins))
+}
