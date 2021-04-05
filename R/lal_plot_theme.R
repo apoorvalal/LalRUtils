eccf87973d3113dceb9ba6369944375c21050c2b
Apoorva Lal <lal.apoorva@gmail.com>
2021-04-04 22:34:26 -0700

    update

--- R/lal_plot_theme.R
+++ R/lal_plot_theme.R
@@ -5,13 +5,22 @@
 #' @examples
 #' plot_ggp_object + lal_plot_theme()
 
-lal_plot_theme <- function(fontfam = "IBM Plex Sans Condensed", fsize = 15, textangle = 0,...) {
+lal_plot_theme = function (fontfam = "IBM Plex Sans Condensed",
+      fsize = 14, textangle = 0, ...) {
   theme_minimal() +
-  theme(
-    text = element_text(family = fontfam, size = fsize),
-    axis.text.x = element_text(angle = textangle),
-    legend.position='bottom',
-    panel.border = element_blank(),
-    ...
-  )
+    theme(
+      text                  = element_text(family = fontfam, size = fsize),
+      axis.text.x           = element_text(angle = textangle),
+      axis.ticks            = element_line(color = "grey92"),
+      plot.title            = element_text(size = 18, face = "bold"),           # title fsize
+      plot.title.position   = "plot",                                           # left align
+      plot.caption          = element_text(size = 9, margin = margin(t = 15)),  # caption fsize
+      plot.caption.position = "plot",                                           # right align
+      plot.subtitle         = element_text(size = 12, color = "grey30"),        # subtitle
+      legend.position       = "top",
+      legend.text           = element_text(color = "grey30"),
+      legend.title          = element_text(size = 12),
+      panel.border          = element_blank(),
+      panel.grid.minor      = element_blank(),
+      ...)
 }
